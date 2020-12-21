0fef024db7e5cbc8f7b42d825cb1ce62c0147720
Kapustin Alexander <akapustin@ambrahealth.com>
2020-12-21 17:50:26 +0300

    Add delete file

---
+++ rcheck/subscribe_multiple/delete.py
@@ -0,0 +1,18 @@
+"""
+Delete queues.
+"""
+
+import pika
+
+parameters = pika.ConnectionParameters(
+    '127.0.0.1',
+    5672,
+    heartbeat=5,
+)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+
+for customer in range(10):
+    channel.queue_delete('sm_dynamic_queue_%s' % str(customer))
+
